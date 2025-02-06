81165e6eb76c145ba3d51d028a21c1960c9ff358
Vladino <vladimir.vrab@bitpanda.com>
2025-02-06 11:35:23 +0100

    Added ability to render font and cleaned the folder&file structure

--- add.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Vladimír Vráb on 19.01.25.
-//
-
-int add(int x, int y) {
-    return x + y;
-}
--- main.cpp
+++ main.cpp
@@ -5,10 +5,14 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-#include "camera.h"
-#include "shader.h"
+#include "src/camera/camera.h"
+#include "src/shaders/shader.h"
 #include "stb_image.h"
-#include "Terrain.h"
+#include "src/terrain/Terrain.h"
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+#include <map>
 
 int add(int x, int y);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
@@ -16,6 +20,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 GLFWwindow* createAndConfigureWindow();
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 unsigned int loadTexture(char const* value);
+void RenderText(Shader &shader, std::string text, float x, float y, float scale, glm::vec3 color);
 
 float deltaTime = 0.0f; // Time between current frame and last frame
 float lastFrame = 0.0f; // Time of last frame
@@ -23,6 +28,9 @@ float lastX = 400, lastY = 300;
 bool firstMouse = false;
 Camera camera(glm::vec3(20.0f, 1.0f, 3.0f));
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+int constexpr  WIDTH = 800;
+int constexpr  HEIGHT = 600;
 
 void processInput(GLFWwindow *window)
 {
@@ -43,6 +51,18 @@ void processInput(GLFWwindow *window)
         camera.ProcessKeyboard(DOWN, deltaTime);
 }
 
+/// Holds all state information relevant to a character as loaded using FreeType
+struct Character {
+    unsigned int TextureID; // ID handle of the glyph texture
+    glm::ivec2   Size;      // Size of glyph
+    glm::ivec2   Bearing;   // Offset from baseline to left/top of glyph
+    unsigned int Advance;   // Horizontal offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
+unsigned int textVAO, textVBO;
+
+
 int main() {
     // create and configure window
     GLFWwindow* window = createAndConfigureWindow();
@@ -51,20 +71,24 @@ int main() {
     }
     // create shader program
     Shader myShader(
-    "/Users/vladino/CLionProjects/mygame/shader.vs",
-    "/Users/vladino/CLionProjects/mygame/shader.fs"
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/shader.vs",
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/shader.fs"
     );
 
     Shader lightShader(
-    "/Users/vladino/CLionProjects/mygame/lightShader.vs",
-    "/Users/vladino/CLionProjects/mygame/lightShader.fs"
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/lightShader.vs",
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/lightShader.fs"
     );
 
     Shader terrainShader(
-    "/Users/vladino/CLionProjects/mygame/shaders/terrainShader.vs",
-    "/Users/vladino/CLionProjects/mygame/shaders/terrainShader.fs"
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/terrainShader.vs",
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/terrainShader.fs"
     );
 
+    Shader textShader(
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/textShader.vs",
+    "/Users/vladino/CLionProjects/mygame/src/shaders/files/textShader.fs"
+    );
 
     glEnable(GL_DEPTH_TEST);
 
@@ -144,8 +168,7 @@ int main() {
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     unsigned int texture = loadTexture("/Users/vladino/CLionProjects/mygame/container.jpg");
-    unsigned int textureGrass = loadTexture("/Users/vladino/CLionProjects/mygame/green-grass.jpg");
-
+    unsigned int textureGrass = loadTexture("/Users/vladino/CLionProjects/mygame/resources/images/green-grass.jpg");
 
     glm::mat4 projection;
 
@@ -182,11 +205,100 @@ int main() {
     // glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *) 0);
     // glEnableVertexAttribArray(0);
 
+    // load font FT_Library ft;
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" <<
+                std::endl;
+    FT_Face face;
+    if (FT_New_Face(ft, "/Users/vladino/CLionProjects/mygame/resources/fonts/Arial.ttf", 0, &face))
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+
+    glm::mat4 textProjection = glm::ortho(0.0f, static_cast<float>(WIDTH), 0.0f, static_cast<float>(HEIGHT));
+    textShader.use();
+    textShader.setMat4("projection", textProjection);
+
+    // load font as face
+    // set size to load glyphs as
+    FT_Set_Pixel_Sizes(face, 0, 48);
+
+    // disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // load first 128 characters of ASCII set
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // Load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // generate texture
+        unsigned int textTexture;
+        glGenTextures(1, &textTexture);
+        glBindTexture(GL_TEXTURE_2D, textTexture);
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RED,
+            face->glyph->bitmap.width,
+            face->glyph->bitmap.rows,
+            0,
+            GL_RED,
+            GL_UNSIGNED_BYTE,
+            face->glyph->bitmap.buffer
+        );
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        Character character = {
+            textTexture,
+            glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+            glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+            static_cast<unsigned int>(face->glyph->advance.x)
+        };
+        Characters.insert(std::pair<char, Character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+    // destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+
+    glGenVertexArrays(1, &textVAO);
+    glGenBuffers(1, &textVBO);
+    glBindVertexArray(textVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6*4, NULL, GL_DYNAMIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Add these variables
+    int frameCount = 0;
+    float fps = 0.0f;
+    float lastTime = 0.0f;
 
     while (!glfwWindowShouldClose(window)) {
         float currentFrame = glfwGetTime();
         deltaTime = currentFrame - lastFrame;
         lastFrame = currentFrame;
+
+        // Calculate FPS
+        frameCount++;
+        if (currentFrame - lastTime >= 1.0f) {
+            fps = frameCount / (currentFrame - lastTime);
+            frameCount = 0;
+            lastTime = currentFrame;
+        }
 
         // input
         // -----
@@ -276,7 +388,10 @@ int main() {
         // render terrain
 
 
-
+        RenderText(textShader, "fps:" + std::to_string((int)fps), 25.0f, 25.0f, 0.4f,
+glm::vec3(0.0f, 0.0f, 0.0f));
+        RenderText(textShader, "gameformykids", WIDTH - 170, HEIGHT - 30, 0.45f,
+        glm::vec3(1, 1, 1));
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -366,7 +481,7 @@ GLFWwindow* createAndConfigureWindow() {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "gameformykids", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -391,3 +506,49 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
+// render line of text
+// -------------------
+void RenderText(Shader &shader, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+    // activate corresponding render state
+    shader.use();
+    glUniform3f(glGetUniformLocation(shader.ID, "textColor"), color.x, color.y, color.z);
+    glActiveTexture(GL_TEXTURE0);
+    glBindVertexArray(textVAO);
+
+    // iterate through all characters
+    std::string::const_iterator c;
+    for (c = text.begin(); c != text.end(); c++)
+    {
+        Character ch = Characters[*c];
+
+        float xpos = x + ch.Bearing.x * scale;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+        float w = ch.Size.x * scale;
+        float h = ch.Size.y * scale;
+        // update VBO for each character
+        float vertices[6][4] = {
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos,     ypos,       0.0f, 1.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+
+            { xpos,     ypos + h,   0.0f, 0.0f },
+            { xpos + w, ypos,       1.0f, 1.0f },
+            { xpos + w, ypos + h,   1.0f, 0.0f }
+        };
+        // render glyph texture over quad
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        // update content of VBO memory
+        glBindBuffer(GL_ARRAY_BUFFER, textVBO);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // be sure to use glBufferSubData and not glBufferData
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        // now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+        x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64 (divide amount of 1/64th pixels by 64 to get amount of pixels))
+    }
+    glBindVertexArray(0);
+    glBindTexture(GL_TEXTURE_2D, 0);
+}
