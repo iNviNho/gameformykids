2c89441a63063976b97e361835911380c3394df0
Vladino <vladimir.vrab@bitpanda.com>
2025-07-08 20:31:36 +0200

    Fix getIsoTime calculation to make sure it is UTC

--- src/utils/Log.cpp
+++ src/utils/Log.cpp
@@ -9,12 +9,16 @@
 std::string Log::getIsoTime() {
     auto now = std::chrono::system_clock::now();
     auto time_t_now = std::chrono::system_clock::to_time_t(now);
-    auto tm_now = *std::localtime(&time_t_now);
+    auto tm_now = *std::gmtime(&time_t_now); // Use std::gmtime for UTC
 
     std::ostringstream oss;
     oss << std::put_time(&tm_now, "%Y-%m-%dT%H:%M:%S");
+
     auto duration = now.time_since_epoch();
-    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
+    auto totalMilliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+    auto totalSeconds = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+    auto milliseconds = totalMilliseconds - (totalSeconds * 1000); // Correct computation of fractional milliseconds
+
     oss << "." << std::setfill('0') << std::setw(3) << milliseconds << "Z";
 
     return oss.str();
