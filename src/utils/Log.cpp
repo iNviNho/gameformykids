0b76e4d4df1e21529528dcf427affdc0b85acd70
Vladino <vladimir.vrab@bitpanda.com>
2025-07-08 09:25:36 +0200

    Log helper class prepends timestamp

--- src/utils/Log.cpp
+++ src/utils/Log.cpp
@@ -1,3 +1,23 @@
 
 
 #include "Log.h"
+#include <string>
+#include <chrono>
+#include <iomanip>
+#include <sstream>
+
+std::string Log::getIsoTime() {
+    auto now = std::chrono::system_clock::now();
+    auto time_t_now = std::chrono::system_clock::to_time_t(now);
+    auto tm_now = *std::localtime(&time_t_now);
+
+    std::ostringstream oss;
+    oss << std::put_time(&tm_now, "%Y-%m-%dT%H:%M:%S");
+    auto duration = now.time_since_epoch();
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
+    oss << "." << std::setfill('0') << std::setw(3) << milliseconds << "Z";
+
+    return oss.str();
+}
+
+
