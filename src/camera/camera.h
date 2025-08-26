2c180d2bdc80692347970a508f7066ac3f938e11
Ján Prokaj <1476863+onthebluedanube@users.noreply.github.com>
2025-08-25 23:01:28 -0400

    Directly compute the view matrix from Yaw, Pitch, Position
    The meaning of Yaw, Pitch has changed, and it is now documented. Removed newly
    unused fields from the Camera class. Replaced with a view matrix that is now
    directly computed in updateViewMatrix() without the use of glm::lookAt().
    
    Use atan() in Player::updateCameraPitch() to avoid computing the hypotenuse.

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -31,13 +31,32 @@ public:
     // camera Attributes
     glm::vec3 Position;
     glm::vec3 TargetPosition;
-    glm::vec3 Front;
-    glm::vec3 Up;
-    glm::vec3 Right;
-    glm::vec3 WorldUp;
-    // euler Angles
+
+    /**
+     * Current transform from the world coordinates to camera coordinates.
+     *
+     * Defined by the camera's Position, and Yaw, Pitch angles that
+     * describe the needed rotation of the world to align with the camera.
+     *
+     * The rotation is defined from Yaw, Pitch by first applying rotation
+     * around the y-axis (Yaw), and then applying rotation around the x-axis
+     * (Pitch).
+     */
+    glm::mat4 viewMatrix;
+
+    /**
+     * Current rotation around the y-axis that brings the world-frame into view, in degrees.
+     */
     float Yaw;
+
+    /**
+     * Target rotation of the world around the Y axis, in degrees.
+     */
     float TargetYaw;
+
+    /**
+     * Current rotation around the x-axis that brings the Yaw-rotated world into view, in degrees.
+     */
     float Pitch;
     // camera options
     float MovementSpeed;
@@ -47,23 +66,22 @@ public:
     // constructor with vectors
     Camera(
         glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f),
         float yaw = YAW,
         float pitch = PITCH
-    ) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+    ) : MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
     {
         Position = position;
         TargetPosition = position;
-        WorldUp = up;
         Yaw = yaw;
         TargetYaw = yaw;
         Pitch = pitch;
+        updateViewMatrix();
     }
 
     // returns the view matrix calculated using Euler Angles and the LookAt Matrix
     glm::mat4 GetViewMatrix() const
     {
-        return glm::lookAt(Position, Position + Front, Up);
+        return viewMatrix;
     }
 
 
@@ -100,10 +118,15 @@ public:
         Position += positionDiff * deltaTime;
 
         float yawDiff = TargetYaw - Yaw;
+        if (yawDiff > 180.0)
+            yawDiff -= 360.0;
+        else if (yawDiff < -180.0)
+            yawDiff += 360.0;
+
         Yaw += yawDiff * deltaTime;
 
         // TODO: Only update once position or yaw diff is so small it doesn't make sense to continue
-        updateCameraVectors();
+        updateViewMatrix();
     }
 
     void UpdatePosition(glm::vec3 position, bool animated) {
@@ -125,7 +148,7 @@ public:
             return;
         }
         Pitch = pitch;
-        updateCameraVectors();
+        updateViewMatrix();
     }
 
     // processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
@@ -139,18 +162,56 @@ public:
     }
 
 private:
-    // calculates the front vector from the Camera's (updated) Euler Angles
-    void updateCameraVectors()
+
+    /**
+     * Calculates the view matrix from the Camera's (updated) Euler Angles
+     *
+     * The matrix is derived as follows:
+     *
+     * const glm::mat3 r_yaw = glm::mat3{ cos_yaw, 0, -sin_yaw,
+     *                                          0, 1,        0,
+     *                                    sin_yaw, 0,  cos_yaw };
+     * const glm::mat3 r_pitch = glm::mat3{ 1,          0,         0,
+     *                                      0,  cos_pitch, sin_pitch,
+     *                                      0, -sin_pitch, cos_pitch };
+     * const glm::mat4 r{ r_pitch * r_yaw };
+     *
+     * viewMatrix = r * glm::mat4{ glm::vec4{1,0,0,0},
+     *                             glm::vec4{0,1,0,0},
+     *                             glm::vec4{0,0,1,0},
+     *                             glm::vec4{-Position.x, -Position.y, -Position.z, 1} } ;
+     */
+    void updateViewMatrix()
     {
-        // calculate the new Front vector
-        glm::vec3 front;
-        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        front.y = sin(glm::radians(Pitch));
-        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        Front = glm::normalize(front);
-        // also re-calculate the Right and Up vector
-        Right = glm::normalize(glm::cross(Front, WorldUp));  // normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-        Up    = glm::normalize(glm::cross(Right, Front));
+        const float cos_pitch = cos(glm::radians(Pitch));
+        const float sin_pitch = sin(glm::radians(Pitch));
+        const float cos_yaw = cos(glm::radians(Yaw));
+        const float sin_yaw = sin(glm::radians(Yaw));
+
+        const float sin_pitch_sin_yaw = sin_pitch * sin_yaw;
+        const float cos_pitch_sin_yaw = cos_pitch * sin_yaw;
+        const float sin_pitch_cos_yaw = sin_pitch * cos_yaw;
+        const float cos_pitch_cos_yaw = cos_pitch * cos_yaw;
+
+        viewMatrix[0][0] = cos_yaw;
+        viewMatrix[0][1] = sin_pitch_sin_yaw;
+        viewMatrix[0][2] = -cos_pitch_sin_yaw;
+        viewMatrix[0][3] = 0.0f;
+
+        viewMatrix[1][0] = 0.0f;
+        viewMatrix[1][1] = cos_pitch;
+        viewMatrix[1][2] = sin_pitch;
+        viewMatrix[1][3] = 0.0f;
+
+        viewMatrix[2][0] = sin_yaw;
+        viewMatrix[2][1] = -sin_pitch_cos_yaw;
+        viewMatrix[2][2] = cos_pitch_cos_yaw;
+        viewMatrix[2][3] = 0.0f;
+
+        viewMatrix[3][0] = -Position.x * cos_yaw - Position.z * sin_yaw;
+        viewMatrix[3][1] = -Position.x * sin_pitch_sin_yaw - Position.y * cos_pitch + Position.z * sin_pitch_cos_yaw;
+        viewMatrix[3][2] = Position.x * cos_pitch_sin_yaw - Position.y * sin_pitch - Position.z * cos_pitch_cos_yaw;
+        viewMatrix[3][3] = 1.0f;
     }
 };
 #endif
